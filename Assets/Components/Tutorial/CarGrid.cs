b65fe9c0522ca2bfd2632c8ad7194f1e91a98725
gamingexpx12 <gamingexpx12@gmail.com>
2017-01-06 19:04:17 +0100

    CarGrid work

--- Assets/Components/Tutorial/CarGrid.cs
+++ Assets/Components/Tutorial/CarGrid.cs
@@ -3,8 +3,23 @@ using System.Collections;
 using System;
 
 public class CarGrid : NavGrid {
+
+    public CarNode nodes;
+    public void CreateGrid()
+    {
+        nodes = GetComponentInChildren<CarNode>();
+    }
     public override Node NodeFromWorldPoint(Vector3 worldPosition)
     {
         throw new NotImplementedException();
+    }
+}
+
+public class CarNode : Node
+{
+    static bool _walkable = true;
+    public CarNode(Vector3 _worldPos, int _gridX, int _gridY) : base(_walkable, _worldPos, _gridX, _gridY)
+    {
+
     }
 }
